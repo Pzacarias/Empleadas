e07d3864eee99f4dd4d09837b0b96659be8169f9
Pzacarias <pzacarias.4@gmail.com>
2021-06-14 19:52:20 -0300

    anotacion

--- src/main/java/ar/com/ada/api/empleadas/EmpleadasApplication.java
+++ src/main/java/ar/com/ada/api/empleadas/EmpleadasApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmpleadasApplication {
 
+	//Se crea el proyecto.
+
 	public static void main(String[] args) {
 		SpringApplication.run(EmpleadasApplication.class, args);
 	}
