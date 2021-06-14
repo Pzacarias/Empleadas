3f695a1d8fd5a89dce76fc9af9f8ab9f629ad879
Pzacarias <pzacarias.4@gmail.com>
2021-06-14 20:29:47 -0300

    Se crean las clases basicas.

--- src/main/java/ar/com/ada/api/empleadas/EmpleadasApplication.java
+++ src/main/java/ar/com/ada/api/empleadas/EmpleadasApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmpleadasApplication {
 
-	//Se crea el proyecto.
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmpleadasApplication.class, args);
